6c601ec3abf0308de9fbb736fd6aa5551b6e6ea8
hussieny <ahmed.m20009@gmail.com>
2023-08-10 10:51:56 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: print Without printf
+ *
+ * Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
